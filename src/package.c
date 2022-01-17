0c8aa5271b11560b3cd98b55c1fe6cb65b7e6513
Mike Brockus <michaelbrockus@gmail.com>
2022-01-16 19:19:32 -0600

    update path to header file

--- src/package.c
+++ src/package.c
@@ -3,7 +3,7 @@
 // author: Michael Brockus
 // gmail: <michaelbrockus@gmail.com>
 //
-#include "miok/package.h"
+#include "miok/queue.h"
 
 //
 // Should return a greeting message as it’s initial value
